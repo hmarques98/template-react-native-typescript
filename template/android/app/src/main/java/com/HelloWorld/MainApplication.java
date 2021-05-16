9211a3b5710d591299eb600fcbdbf8030da4da7f
Henrique Marques <marquesprogrammer@hotmail.com>
2021-05-16 11:10:35 -0300

    chore: name id android settigs

--- template/android/app/src/main/java/com/HelloWorld/MainApplication.java
+++ template/android/app/src/main/java/com/HelloWorld/MainApplication.java
@@ -1,4 +1,4 @@
-package com.HelloWorld;
+package com.example.helloWorld;
 
 import android.app.Application;
 import android.content.Context;
@@ -74,7 +74,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.HelloWorld.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.example.helloWorld.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
