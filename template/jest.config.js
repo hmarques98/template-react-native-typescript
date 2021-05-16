2129c98d6cf423c73d9f881c286cfef67b9860b8
Henrique Marques <marquesprogrammer@hotmail.com>
2021-05-16 05:58:25 -0300

    chore: updates files and library to testing library

--- template/jest.config.js
+++ template/jest.config.js
@@ -1,7 +1,22 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const jestPreset = require('@testing-library/react-native/jest-preset');
 
 module.exports = {
-  preset: 'jest-expo',
-  setupFiles: [...jestPreset.setupFiles],
-  setupFilesAfterEnv: ['./jest.setup.js'],
+  testEnvironment: 'node',
+  preset: 'react-native',
+  setupFiles: [
+    './jest.setup.js',
+    './node_modules/react-native-gesture-handler/jestSetup.js',
+    './node_modules/react-native/jest/setup.js',
+    './node_modules/react-native/Libraries/Utilities/__mocks__/BackHandler.js',
+  ],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  clearMocks: true,
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/assetsTransformer.js',
+    '\\.(css|less)$': '<rootDir>/assetsTransformer.js',
+    '\\.svg': '<rootDir>/__mocks__/svgMocks.js',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
--- template/jest.setup.js
+++ template/jest.setup.js
@@ -1,5 +1,9 @@
 import mockRNCNetInfo from '@react-native-community/netinfo/jest/netinfo-mock.js';
 import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+import React from 'react';
+import '@testing-library/jest-native/extend-expect';
+import { jest } from '@jest/globals';
+import { server } from './src/test/mocks/server';
 
 jest.mock('@react-native-community/netinfo', () => mockRNCNetInfo);
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
@@ -9,6 +13,7 @@ jest.mock('@sentry/react-native', () => ({
 }));
 
 jest.mock('react-native-reanimated', () => {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
   const Reanimated = require('react-native-reanimated/mock');
 
   // The mock for `call` immediately calls the callback which is incorrect
@@ -32,6 +37,63 @@ global.requestAnimationFrame = (cb) => {
 };
 
 jest.mock('react-native/Libraries/Components/Switch/Switch', () => {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
   const mockComponent = require('react-native/jest/mockComponent');
   return mockComponent('react-native/Libraries/Components/Switch/Switch');
+});
+
+jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
+jest.mock('react-native-gesture-handler', () => {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const View = require('react-native/Libraries/Components/View/View');
+  return {
+    Swipeable: View,
+    DrawerLayout: View,
+    State: {},
+    ScrollView: View,
+    Slider: View,
+    Switch: View,
+    TextInput: View,
+    ToolbarAndroid: View,
+    ViewPagerAndroid: View,
+    DrawerLayoutAndroid: View,
+    WebView: View,
+    NativeViewGestureHandler: View,
+    TapGestureHandler: View,
+    FlingGestureHandler: View,
+    ForceTouchGestureHandler: View,
+    LongPressGestureHandler: View,
+    PanGestureHandler: View,
+    PinchGestureHandler: View,
+    RotationGestureHandler: View,
+    /* Buttons */
+    RawButton: View,
+    BaseButton: View,
+    RectButton: View,
+    BorderlessButton: View,
+    /* Other */
+    FlatList: View,
+    gestureHandlerRootHOC: () => null,
+    Directions: {},
+  };
+});
+
+//establish api mocking before all tests
+beforeAll(() => server.listen());
+
+beforeEach(() => {
+  global.fetch = jest.fn((...args) => {
+    console.warn('global.fetch needs to be mocked in tests', ...args);
+    throw new Error('global.fetch needs to be mocked in tests');
+  });
+});
+
+//clean up after the tests are finished
+afterAll(() => server.close());
+
+afterEach(() => {
+  global.fetch.mockRestore();
+  //reset any requests handlers that we may add during the tests,
+  //so they don't affect other tests.
+  server.resetHandlers();
 });
