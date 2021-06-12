b2eb28df382ef54cdc3e7eaecfd6e0782f7bb1e7
Henrique Marques <marquesprogrammer@hotmail.com>
2021-06-12 16:00:44 -0300

    chore: plopjs

--- template/plopfile.js
+++ template/plopfile.js
@@ -47,4 +47,53 @@ module.exports = (plop) => {
       },
     ],
   });
+
+  plop.setGenerator('screen', {
+    description: 'Create a common screen',
+    // User input prompts provided as arguments to the template
+    prompts: [
+      {
+        // Raw text input
+        type: 'input',
+        // Variable name for this input
+        name: 'name',
+        // Prompt to display on command line
+        message: 'What is your common screen name?',
+      },
+    ],
+    actions: [
+      {
+        // Action type 'append' injects a template into an existing file
+        type: 'append',
+        path: 'src/screens/index.ts',
+        // Pattern tells plop where in the file to inject the template
+        pattern: `/* PLOP_INJECT_SCREEN */`,
+        template: `{{pascalCase name}}: { component: {{pascalCase name}}Screen },`,
+      },
+      {
+        // Action type 'append' injects a template into an existing file
+        type: 'append',
+        path: 'src/screens/index.ts',
+        // Pattern tells plop where in the file to inject the template
+        pattern: `/* PLOP_INJECT_IMPORT */`,
+        template: `import {{pascalCase name}}Screen from 'screens/{{pascalCase name}}';`,
+      },
+      {
+        // Action type 'append' injects a template into an existing file
+        type: 'append',
+        path: 'src/screens/index.ts',
+        // Pattern tells plop where in the file to inject the template
+        pattern: `/* PLOP_INJECT_TYPE */`,
+        template: `{{pascalCase name}}: undefined;`,
+      },
+      {
+        // Add a new file
+        type: 'add',
+        // Path for the new file
+        path: 'src/screens/{{pascalCase name}}.tsx',
+        // Handlebars template used to generate content of new file
+        templateFile: 'plop-templates/CommonScreen.js.hbs',
+      },
+    ],
+  });
 };
