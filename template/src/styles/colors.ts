db92110e653bf2edda9fb8891d95dd568649072d
Henrique Marques <marquesprogrammer@hotmail.com>
2021-06-20 18:59:55 -0300

    chore: removed comments

--- template/src/styles/colors.ts
+++ template/src/styles/colors.ts
@@ -1,14 +1,11 @@
-// DEFAULT
 export const PRIMARY = '#272C30';
 export const SECONDARY = '#FFE300';
 export const BACKGROUND_COLOR = '#1B1F23';
 export const WHITE = '#C8C8C8';
 export const BLACK = '#3D4247';
 
-// ACTIONS
 export const SUCCESS = '#16BD04';
 export const WARNING = '#ffae00';
 export const ALERT = '#cc4b37';
 
-// GRAYSCALE
 export const GRAY_LIGHT = '#44494F';
